98828d6089cc7c0736accc709cc6c276bfce9925
jonmackeyparty <jon@jonmackey.party>
2019-04-05 14:07:30 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validate :clickbaits
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+  private
+
+  def clickbaits
+    if title
+      titler = title.split(" ")
+      clickers = %w(Believe Secret Top Guess)
+      if (titler & clickers).empty?
+        errors.add(:title, "needs more clickbait")
+      end
+    end
+  end
+
 end
