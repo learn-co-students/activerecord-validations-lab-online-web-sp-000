db361ce9ad092d8410598698f0d9551dbd8b458c
Btate712 <btate712@gmail.com>
2019-05-12 14:40:27 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: {in: %w( Fiction Non-Fiction ),
+    :message => "%{value} is not a valid category" }
+  validate :click_baity
+
+  def click_baity
+    errors.add(:title, "is not adequately click_baity") unless self.title &&
+      (self.title.include?("Won't Believe") ||
+      self.title.include?("Secret") ||
+      self.title.include?("Top ") ||
+      self.title.include?("Guess"))
+  end
 end
