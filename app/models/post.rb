cba716141c031362ecf8606fa9541c0f1dcdcdb7
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-16 21:13:17 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+class TitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    click_baits = [/Won't Believe/, /Secret/, /Guess/, /Top \d/]
+    unless click_baits.any?{|regex| value =~ regex}
+      record.errors[attribute] << (options[:message] || "is not clickbait-y enough")
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true, title: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
