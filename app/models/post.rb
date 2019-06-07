d14c566b7795dd3bbe0a83d0eaa713538c49a9d4
Jeff Benton <sikletz@gmail.com>
2019-06-07 18:50:53 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?, on: :create
+  
+  def is_clickbait?
+    if title
+      errors.add(:title, "is not clickbait") unless title.match? /Won't Believe|Secret|Top [0-9]+|Guess/
+    end
+  end
 end
