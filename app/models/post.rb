94b500362f1f22aaa2f9ef4f890f5482224f16a9
blainelawson <blaine.lawson@gmail.com>
2019-06-14 08:18:00 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,16 +3,28 @@ class Post < ActiveRecord::Base
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-    validate :valid_title
+    validate :is_clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+    ]
 
     private
 
-    def valid_title
-        # binding.pry
-        if self.title.present? && !self.title.include?("Won't Believe") && !self.title.include?("Secret") && !self.title.include?("Top") && !self.title.include?("Guess")
-        # if self.title.present? && !self.title.include?%w("Won't Believe" "Secret" "Top" "Guess")
-        #  binding.pry
-            errors.add(:title, "Invalid Title")            
+    # def valid_title
+    #     # binding.pry
+    #     if self.title.present? && !self.title.include?("Won't Believe") && !self.title.include?("Secret") && !self.title.include?("Top") && !self.title.include?("Guess")
+    #     # if self.title.present? && !self.title.include?%w("Won't Believe" "Secret" "Top" "Guess")
+    #     #  binding.pry
+    #         errors.add(:title, "Invalid Title")            
+    #     end
+
+    def is_clickbait?
+        if CLICKBAIT_PATTERNS.none? {|pat| pat.match title }
+            errors.add(:title, "Must be clickbait")
         end
     end
 
