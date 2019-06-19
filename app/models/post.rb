57d726700a518e6f087c2b3a48f8dd97d4937e8f
jei-doubleton <jennifer.singleton@gmail.com>
2019-06-18 20:40:15 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait?
+
+    def clickbait?
+        if title != nil && !title.include?("Won't Believe" || "Secret" || "Guess" || "Top" + "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "0")
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
