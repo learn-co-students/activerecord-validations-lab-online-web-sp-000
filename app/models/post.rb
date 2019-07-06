4f41cb2296bedd7f40e968a95b6aba80a6e541f4
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-06 01:54:34 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
-class Post < ActiveRecord::Base
+class ClickbaityValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    # pattern matcher =~ operator
+    # /\d/ - A digit character ([0-9])
+    unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
+      record.errors[attribute] << ("Not a clickbait title")
+    end
+  end
 end
+
+class Post < ActiveRecord::Base
+  validates :title, presence: true, clickbaity: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction ) }
+
+end
+
+
+
