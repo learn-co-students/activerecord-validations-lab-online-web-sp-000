c20ad1cb359a12757bb3e79ab5ec021d27cd5756
Jared Grove <jaredkgrove@gmail.com>
2019-08-06 14:13:04 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :click_baity?
+
+    def click_baity?
+        if !!self.title && !self.title.match(/(won't believe) | (secret) | (guess) | (top \d)/i)
+            errors.add(:title, "Title must be click-bait")
+        end
+    end
 end
