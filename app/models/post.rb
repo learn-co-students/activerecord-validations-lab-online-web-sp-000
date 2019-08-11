27c9a98246178623178d52c6d964804612179997
Jim <teachlife@outlook.com>
2019-08-10 20:39:03 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :cannot_be_clickbait
+
+
+  def cannot_be_clickbait
+    if self.title == "True Facts"
+      errors.add(:click_bait, "can't be click_bait")
+    end
+  end
+
 end
