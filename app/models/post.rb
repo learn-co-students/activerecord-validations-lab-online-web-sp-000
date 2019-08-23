8abf25d716764641f915558245e0cc1b6a5c07c7
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-23 02:01:16 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: {in: ["Fiction", "Non-Finction"]}
+  validate :clickbait
+
+  def clickbait
+    if !title.nil? && !title.include?("You Won't" || "Secret" || "Top" || "Guess")
+       errors[:name] << 'Need a better title please!'
+     end
+   end
+
 end
